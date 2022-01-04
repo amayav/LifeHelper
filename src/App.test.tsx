c151c7c4a15942327b669c164b7031227ba22b2a
amayav <vamayav@yahoo.co.jp>
2022-01-04 14:52:25 +0900

    simple timeline is added (#5)
    * simple timeline is added
    
    * typo of workflow is fixed
    
    * build error is fixed
    
    * arrange tables location

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@
 //import { render, screen, fireEvent } from '@testing-library/react';
 import { unmountComponentAtNode } from "react-dom";
 import App from './App';
-import { Idols, Skill, Data } from './App';
+import { Idols, Skill, Data, SimpleData } from './App';
 import {PERFECT_SUPPORT_3, PERFECT_SUPPORT_2, PERFECT_SUPPORT_1, SKILL_BOOST, DAMAGE_GUARD, ENCORE, CINDERELLA_MAGIC, OTHER} from './App';
 
 let container: any = null
@@ -19,8 +19,12 @@ afterEach(() => {
 
 class TestIdols extends Idols {
   public testUpdateTimeLine(startTime: number, skills: Skill[], music_time: number = 120, is_resonance: boolean = false) {
-    return this.updateTimeLine(startTime, skills, music_time, is_resonance)
+    return super.updateTimeLine(startTime, skills, music_time, is_resonance)
   }
+  public testGetSimpleTimeLine() {
+    return super.getSimpleTimeLine()
+  }
+
   public setResonace(is_resonance: boolean): void {
     this.setState({is_resonance: is_resonance})
   }
@@ -626,4 +630,30 @@ test('resonance perfect support 1 + skill boost * 3', () => {
   data = t.testUpdateTimeLine(8, skills, 120, true)
   expect_data = {...expect_data, perfect: "p"}
   expect(data).toEqual(expect_data)
+})
+
+test('simple timeline', () => {
+  const t = new TestIdols({} as any)
+  const damage_guard_skill: Skill = {name: DAMAGE_GUARD, interval: 12, time: "time_e"}
+  let skills: Skill[] = [ perfect_support_3_skill, skill_boost_skill, damage_guard_skill, other_skill, other_skill, ]
+
+  const timeList : number[] = [...Array(20*2)].map((_i, i) => i/2)
+
+  let data: Data[] = timeList.map(startTime => {
+      return t.updateTimeLine(startTime, skills, 20, false)
+  })
+
+  const simple_timeline: SimpleData[] = t.testGetSimpleTimeLine()
+  const expect_timeline: SimpleData[] = [
+  {start: "0.0 - 8.0", time: "8.0", mode: ""},
+  {start: "8.0 - 11.5", time: "3.5", mode: "p"},
+  {start: "11.5 - 12.0", time: "0.5", mode: ""},
+  {start: "12.0 - 16.0", time: "4.0", mode: "g"},
+  {start: "16.0 - 19.5", time: "3.5", mode: "p"},
+  ]
+  /* the last part is calculated in another place
+  {start: "19.5 - 20.0", time: "0.5", mode: "g"}
+  */
+
+  expect(simple_timeline).toEqual(expect_timeline)
 })
